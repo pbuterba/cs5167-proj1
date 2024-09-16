4a1bbc6e25ffe06eaef48d72757ff650000a8910
pbuterba <pbuterba.corp@gmail.com>
2024-09-15 20:29:01 -0400

    Add dummy logged activities

---
+++ src/data/activities.js
@@ -0,0 +1,70 @@
+export const activityData = {
+    '09-15-2024': [
+        {
+            'name': 'Church',
+            'time': '10:15',
+            'duration': 90
+        },
+        {
+            'name': 'AI Homework Meeting',
+            'time': '20:00',
+            'duration': 150
+        }
+    ],
+    '09-14-2024': [
+        {
+            'name': 'AI Homework Meeting',
+            'time': '20:00',
+            'duration': 150
+        }
+    ],
+    '09-13-2024': [
+        {
+            'name': 'ENED Section 016',
+            'time': '8:00',
+            'duration': 110
+        },
+        {
+            'name': 'ENED Section 018',
+            'time': '10:00',
+            'duration': 110
+        },
+        {
+            'name': 'User Interface Class',
+            'time': '13:25',
+            'duration': 55
+        },
+        {
+            'name': 'AI Homework Meeting',
+            'time': '21:00',
+            'duration': 150
+        }
+    ],
+    '09-12-2024': [
+        {
+            'name': 'Illustrator and Photoshop Class',
+            'time': '9:30',
+            'duration': 80
+        },
+        {
+            'name': 'AI Principles Class',
+            'time': '11:00',
+            'duration': 80
+        },
+        {
+            'name': 'Automata Class',
+            'time': '14:00',
+            'duration': 80
+        },
+        {
+            'name': 'Tutor Room Shift',
+            'time': '16:40',
+            'duration': 125
+        },
+        {
+            'name': 'AI Homework Meeting',
+            'time': '20:00',
+            'duration': 150
+        }
+    ]
+};
