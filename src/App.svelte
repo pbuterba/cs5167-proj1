b505be4477436387d2d3b249e297aac1b928cb4d
pbuterba <pbuterba.corp@gmail.com>
2024-09-15 20:27:28 -0400

    Add day editor

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,16 @@
 <script>
-  import Header from './lib/Header.svelte';
+    import Header from './lib/Header.svelte';
+    import DayEditor from './lib/DayDisplay/DayEditor.svelte';
+    import {getDateSlug} from './lib/utility-functions.js';
+    import {activityData} from './data/activities.js';
+
+    let loggedActivities = activityData;
+
+    let currDate = getDateSlug(new Date());
+    let currDayActivities = currDate in loggedActivities ? loggedActivities['09-12-2024'] : [];
 </script>
 
 <div class="header">
-  <Header />
+    <Header />
+    <DayEditor date={new Date()} dayActivities={currDayActivities} />
 </div>
