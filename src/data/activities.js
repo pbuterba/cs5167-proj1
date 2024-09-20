a2a7a0ac3f1ec47e5a86b3102167ca94144d30cb
pbuterba <pbuterba.corp@gmail.com>
2024-09-19 21:15:31 -0400

    Add activities for 9/17, 9/18, 9/19, and 9/20

--- src/data/activities.js
+++ src/data/activities.js
@@ -1,4 +1,120 @@
 export const activityData = {
+    '09-20-2024': [
+        {
+            'name': 'ENED Section 016',
+            'time': '8:00',
+            'duration': 110,
+            'category': 'TA Work'
+        },
+        {
+            'name': 'ENED Section 018',
+            'time': '10:00',
+            'duration': 110,
+            'category': 'TA Work'
+        },
+        {
+            'name': 'User Interface Class',
+            'time': '13:25',
+            'duration': 55,
+            'category': 'Class'
+        },
+        {
+            'name': 'Blood Donation',
+            'time': '14:45',
+            'duration': 120,
+            'category': 'Other'
+        }
+    ],
+    '09-19-2024': [
+        {
+            'name': 'Illustrator and Photoshop Class',
+            'time': '9:30',
+            'duration': 80,
+            'category': 'Class'
+        },
+        {
+            'name': 'AI Principles Class',
+            'time': '11:00',
+            'duration': 110,
+            'category': 'Class'
+        },
+        {
+            'name': 'Career Fair',
+            'time': '12:30',
+            'duration': 60,
+            'category': 'Job Search'
+        },
+        {
+            'name': 'Automata Class',
+            'time': '2:00',
+            'duration': 110,
+            'category': 'Class'
+        },
+        {
+            'name': 'ENED Tutoring Shift',
+            'time': '16:40',
+            'duration': 125,
+            'category': 'TA Work'
+        }
+    ],
+    '09-18-2024': [
+        {
+            'name': 'ENED Section 016',
+            'time': '8:00',
+            'duration': 110,
+            'category': 'TA Work'
+        },
+        {
+            'name': 'ENED Section 018',
+            'time': '10:00',
+            'duration': 110,
+            'category': 'TA Work'
+        },
+        {
+            'name': 'Career Fair',
+            'time': '12:00',
+            'duration': 75,
+            'category': 'Job Search'
+        },
+        {
+            'name': 'User Interface Class',
+            'time': '13:25',
+            'duration': 55,
+            'category': 'Class'
+        },
+        {
+            'name': 'City Group',
+            'time': '18:30',
+            'duration': 150,
+            'category': 'Church'
+        }
+    ],
+    '09-17-2024': [
+        {
+            'name': 'Illustrator and Photoshop Class',
+            'time': '9:30',
+            'duration': 110,
+            'category': 'Class'
+        },
+        {
+            'name': 'AI Principles Class',
+            'time': '11:00',
+            'duration': 110,
+            'category': 'Class'
+        },
+        {
+            'name': 'Automata Class',
+            'time': '2:00',
+            'duration': 110,
+            'category': 'Class'
+        },
+        {
+            'name': 'Senior Design Meeting',
+            'time': '3:30',
+            'duration': 90,
+            'category': 'Homework'
+        }
+    ],
     '09-16-2024': [
         {
             'name': 'User Interface Class',
--- src/data/color-mappings.js
+++ src/data/color-mappings.js
@@ -2,5 +2,7 @@ export const colorMappings = {
     'Church': '#F1C232',
     'Homework': '#FFF2CC',
     'Class': '#FFFF00',
-    'TA Work': '#6AA84F'
+    'TA Work': '#6AA84F',
+    'Job Search': '#FFD966',
+    'Other': '#999999'
 }
