ed597032ab540814b659b09951074ebd6d8459fd
pbuterba <pbuterba.corp@gmail.com>
2024-09-21 18:23:13 -0400

    Add activities through the end of the month

--- src/data/activities.js
+++ src/data/activities.js
@@ -1,4 +1,155 @@
 export const activityData = {
+    '09-30-2024': [
+        {
+            'name': 'User Interface Class',
+            'time': '13:25',
+            'duration': 55,
+            'category': 'Class'
+        },
+        {
+            'name': 'ENED TA Meeting',
+            'time': '15:35',
+            'duration': 55,
+            'category': 'TA Work'
+        }
+    ],
+    '09-29-2024': [
+        {
+            'name': 'Church',
+            'time': '8:00',
+            'duration': 240,
+            'category': 'Church'
+        }
+    ],
+    '09-28-2024': [],
+    '09-27-2024': [
+        {
+            'name': 'ENED Section 016',
+            'time': '8:00',
+            'duration': 110,
+            'category': 'TA Work'
+        },
+        {
+            'name': 'ENED Section 018',
+            'time': '10:00',
+            'duration': 110,
+            'category': 'TA Work'
+        },
+        {
+            'name': 'User Interface Class',
+            'time': '13:25',
+            'duration': 55,
+            'category': 'Class'
+        }
+    ],
+    '09-26-2024': [
+        {
+            'name': 'Illustrator and Photoshop Class',
+            'time': '9:30',
+            'duration': 80,
+            'category': 'Class'
+        },
+        {
+            'name': 'AI Principles Class',
+            'time': '11:00',
+            'duration': 80,
+            'category': 'Class'
+        },
+        {
+            'name': 'Automata Class',
+            'time': '14:00',
+            'duration': 80,
+            'category': 'Class'
+        },
+        {
+            'name': 'Tutor Room Shift',
+            'time': '16:40',
+            'duration': 125,
+            'category': 'TA Work'
+        }
+    ],
+    '09-25-2024': [
+        {
+            'name': 'ENED Section 016',
+            'time': '8:00',
+            'duration': 110,
+            'category': 'TA Work'
+        },
+        {
+            'name': 'ENED Section 018',
+            'time': '10:00',
+            'duration': 110,
+            'category': 'TA Work'
+        },
+        {
+            'name': 'User Interface Class',
+            'time': '13:25',
+            'duration': 55,
+            'category': 'Class'
+        },
+        {
+            'name': 'City Group',
+            'time': '18:30',
+            'duration': 150,
+            'category': 'Church'
+        }
+    ],
+    '09-24-2024': [
+        {
+            'name': 'Illustrator and Photoshop Class',
+            'time': '9:30',
+            'duration': 110,
+            'category': 'Class'
+        },
+        {
+            'name': 'AI Principles Class',
+            'time': '11:00',
+            'duration': 110,
+            'category': 'Class'
+        },
+        {
+            'name': 'Automata Class',
+            'time': '2:00',
+            'duration': 110,
+            'category': 'Class'
+        },
+        {
+            'name': 'Senior Design Meeting',
+            'time': '3:30',
+            'duration': 90,
+            'category': 'Homework'
+        }
+    ],
+    '09-23-2024': [
+        {
+            'name': 'User Interface Class',
+            'time': '13:25',
+            'duration': 55,
+            'category': 'Class'
+        },
+        {
+            'name': 'ENED TA Meeting',
+            'time': '15:35',
+            'duration': 55,
+            'category': 'TA Work'
+        }
+    ],
+    '09-22-2024': [
+        {
+            'name': 'Church',
+            'time': '10:15',
+            'duration': 105,
+            'category': 'Church'
+        }
+    ],
+    '09-21-2024': [
+        {
+            'name': 'AI Homework Meeting',
+            'time': '20:00',
+            'duration': 60,
+            'category': 'Homework'
+        }
+    ],
     '09-20-2024': [
         {
             'name': 'ENED Section 016',
