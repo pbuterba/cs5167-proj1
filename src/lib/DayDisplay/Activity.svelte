8f4eae80a0602f05fd05be14aee5e92c7318efdf
pbuterba <pbuterba.corp@gmail.com>
2024-09-21 23:35:19 -0400

    Add "Edit" and "Delete" buttons

--- src/lib/DayDisplay/Activity.svelte
+++ src/lib/DayDisplay/Activity.svelte
@@ -1,25 +1,67 @@
 <script>
+    import {createEventDispatcher} from 'svelte';
     import {toTwelveHourTime} from '../utility-functions.js'
     import {colorMappings} from '../../data/color-mappings.js';
 
     export let activityData;
+
+    const dispatch = createEventDispatcher();
+
+    let focused = false;
+
+    //Determine colors
+    let color = colorMappings[activityData.category];
+    let channelSum = 0;
+    for(let i = 1; i <= 5; i += 2) {
+        let channel = parseInt(color.substring(i, i + 2), 16);
+        channelSum += channel;
+    }
+    let channelAverage = channelSum/3;
+    let contrastColor = channelAverage < 127 ? "white" : "black";
+    let textColor = channelAverage < 127 ? "black" : "white";
+
+    function getChannelContrast(value) {
+        value = value - 127;
+        if(value < 0) {
+            value = 255 + value;
+        }
+        return value;
+    }
+
+    //Edit and delete functions
+    function editActivity() {
+        dispatch('editActivity', activityData);
+    }
+    function deleteActivity() {
+
+    }
 </script>
 
-<main style={"background-color: " + colorMappings[activityData.category]}>
-    <p>{toTwelveHourTime(activityData.time, false)}</p>
-    <h1>{activityData.name}</h1>
-    <p class="duration">{activityData.duration}m</p>
+<main on:mouseenter={() => {focused = true}} on:mouseleave={() => {focused = false}} style={"background-color: " + color}>
+    <div id="edit-buttons">
+        {#if focused}
+            <button style={"background-color: " + contrastColor + "; color: " + textColor} on:click={editActivity}>Edit</button>
+            <button style={"background-color: " + contrastColor + "; color: " + textColor} on:click={deleteActivity}>Delete</button>
+        {/if}
+    </div>
+    <div id="activity-info">
+        <p>{toTwelveHourTime(activityData.time, false)}</p>
+        <h1>{activityData.name}</h1>
+        <p class="duration">{activityData.duration}m</p>
+    </div>
 </main>
 
 <style>
     main {
         border: 1px solid black;
         margin: 0.5vh 0.5vw;
+        height: 8vh;
+    }
+    #activity-info {
         display: flex;
         justify-content: space-between;
-        height: 5vh;
     }
-    main * {
+    #activity-info * {
         margin: auto 0.6vw;
     }
     h1 {
@@ -27,5 +69,20 @@
     }
     .duration {
         font-style: italic;
+    }
+    #edit-buttons {
+        display: flex;
+        justify-content: flex-end;
+        padding: 0.5vh 0.5vw;
+        min-height: 2vh;
+    }
+    #edit-buttons button {
+        display: flex;
+        align-items: center;
+        height: 2vh;
+        font-size: 0.8rem;
+        padding: 0.25vh 0.5vw;
+        margin: 0 0.25vw;
+        border-radius: 0;
     }
 </style>
