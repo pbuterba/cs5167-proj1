174bb6c35bb7f2edd11d067a68a1efab9e422cb9
pbuterba <pbuterba.corp@gmail.com>
2024-09-23 19:55:16 -0400

    Make activities store, add goals

---
+++ src/data/goals.js
@@ -0,0 +1,5 @@
+export const goals = [
+    {'category': 'Homework', 'time': 360, 'type': 'day', 'period': '09-23-2024'},
+    {'category': 'Class', 'time': 55, 'type': 'day', 'period': '09-23-2024'},
+    {'category': 'TA Work', 'time': 720, 'type': 'week', 'period': '09-22-2024'},
+];
--- src/stores.js
+++ src/stores.js
@@ -1,6 +1,7 @@
 import {readable, writable, derived} from 'svelte/store';
 import {getDateSlug} from './lib/utility-functions.js';
 import {activityData} from './data/activities.js';
+import {goals} from './data/goals.js';
 
 //Time store code sourced from learn.svelte.dev tutorial: https://learn.svelte.dev/tutorial/readable-stores
 export const time = readable(new Date(), function start(set) {
@@ -38,3 +39,4 @@ export const elapsedTime = derived(
 export const selectedDate = writable(getDateSlug(new Date()));
 
 export const storedActivities = writable(activityData);
+export const storedGoals = writable(goals);
