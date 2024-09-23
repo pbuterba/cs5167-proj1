c4e693c0766c5f2700a713ca3502ab8326e76e25
pbuterba <pbuterba.corp@gmail.com>
2024-09-22 23:11:40 -0400

    Renamed "color-mappings" to "categories"

---
+++ src/data/categories.js
@@ -0,0 +1,8 @@
+export const categories = {
+    "Church": "#F1C232",
+    "Class": "#FFFF00",
+    "Homework": "#FFF2CC",
+    "Job Search": "#FFD966",
+    "Other": "#999999",
+    "TA Work": "#6AA84F"
+}
